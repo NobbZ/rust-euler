ac88a721de15e170ed8c172643ce1740903c28ad
Norbert Melzer <timmelzer@gmail.com>
2015-01-21 22:42:29 +0100

    Adding stopwatch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate euler;
+extern crate stopwatch;
 
 use std::os;
 
@@ -7,10 +8,14 @@ use euler::problems;
 fn main () {
   let args = os::args();
 
+  let sw = stopwatch::Stopwatch::start_new();
+
   println!("Result of problem {:>3}: {:>15}", args[1], match args[1].as_slice() {
     "1" => problems::problem001(),
     "2" => problems::problem002(),
     "3" => problems::problem003(),
     _   => 0
   });
+
+  println!("Calculation took {}ms", sw.elapsed_ms());
 }
