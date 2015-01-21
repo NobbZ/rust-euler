39bbf80c7574fbd3fe7fd6bbba660ca7fe210070
Norbert Melzer <timmelzer@gmail.com>
2015-01-21 22:20:59 +0100

    some work on tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,11 +1,24 @@
 extern crate euler;
 
+use euler::problems as p;
+use euler::tools as t;
+
 #[test]
 fn problem001 () {
-  assert_eq!(233168, euler::problems::problem001());
+  assert_eq!(233168, p::problem001());
 }
 
 #[test]
 fn problem002 () {
-  assert_eq!(4613732, euler::problems::problem002());
+  assert_eq!(4613732, p::problem002());
+}
+
+
+#[test]
+fn fib () {
+  assert_eq!(0, t::fib(0));
+  assert_eq!(1, t::fib(1));
+  assert_eq!(1, t::fib(2));
+  assert_eq!(2, t::fib(3));
+}
 }
